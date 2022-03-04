9e8384af14046503bdca15521d500eea98b24f80
Jakub Borkowski <bojakub@wp.pl>
2022-03-04 12:32:14 +0100

    Improved exception handling

--- src/Client.java
+++ src/Client.java
@@ -1,27 +1,58 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
 
-    public static void main(String[] args) throws Exception {
-        Socket socket = new Socket("localhost", 2011);
-        DataInputStream in = new DataInputStream(socket.getInputStream());
-        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-        Scanner read = new Scanner(System.in);
-        String clientmsg, servermsg; //wiadomość od klienta do serwera, wiadomość od serwera do klienta
-        //clientmsg = "key_in-get{test};";
-        while (true) {
-            clientmsg = read.nextLine();
-            out.writeUTF(clientmsg);
-            if (clientmsg.equals("exit"))
-                break;
-            servermsg = in.readUTF();
-            System.out.println("Server: " + servermsg);
-        }
+    private static void closeConnection(DataInputStream in, DataOutputStream out, Socket socket) throws IOException {
         in.close();
         out.close();
         socket.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Waiting for connection...");
+        Socket socket = null;
+        boolean connected = false;
+        while(!connected){
+            try {
+                socket = new Socket("localhost", 2011);
+                connected = true;
+            } catch (IOException e) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+        System.out.println("Connected to server.");
+        DataInputStream in = new DataInputStream(socket.getInputStream());
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        Scanner read = new Scanner(System.in);
+        String clientmsg, servermsg = ""; //wiadomość od klienta do serwera, wiadomość od serwera do klienta
+        while (true) {
+            clientmsg = read.nextLine();
+            try {
+                out.writeUTF(clientmsg);
+            } catch (IOException e) {
+                System.out.println("Connection lost!");
+                closeConnection(in, out, socket);
+                System.exit(1);
+            }
+            if (clientmsg.equals("exit"))
+                break;
+            try {
+                servermsg = in.readUTF();
+            } catch (IOException e) {
+                System.out.println("Connection lost!");
+                closeConnection(in, out, socket);
+                System.exit(1);
+            }
+            System.out.println("Server: " + servermsg);
+        }
+        closeConnection(in, out, socket);
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,6 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -8,50 +9,74 @@ import java.util.regex.Pattern;
 
 public class Server {
 
-    public static void main(String[] args) throws Exception {
-        ArrayList<String> keys = new ArrayList<String>(); //baza kluczy
+    private static final ArrayList<String> keys = new ArrayList<>(); //baza kluczy
+    private static final String password = "haslo"; //hasło może zawierać znaki, cyfry i podkreślenie
+
+    private static String processMessage(String clientmsg){
+        String servermsg;
+        Pattern pattern = Pattern.compile("key_in-(get|set)\\{(\\w*):?(\\w*)};");
+        Matcher matcher = pattern.matcher(clientmsg);
+        if (matcher.find()) {
+            servermsg = "key_out-{";
+            if (matcher.group(1).equals("get")) {
+                servermsg += matcher.group(2);
+                if (keys.contains(matcher.group(2)))
+                    servermsg += ":yes};";
+                else
+                    servermsg += ":not};";
+            } else if (matcher.group(1).equals("set")) {
+                servermsg += matcher.group(3);
+                if (matcher.group(2).equals(password)) {
+                    keys.add(matcher.group(3));
+                    servermsg += ":yes};";
+                } else
+                    servermsg += ":not};";
+            }
+        } else {
+            servermsg ="key_out-{" + clientmsg + ":error};";
+        }
+        return servermsg;
+    }
+
+    private static void closeConnection(DataInputStream in, DataOutputStream out,
+                                        Socket socket, ServerSocket serverSocket) throws IOException {
+        in.close();
+        out.close();
+        socket.close();
+        serverSocket.close();
+    }
+
+    public static void main(String[] args) throws IOException {
         keys.add("test"); //testowy klucz
-        Pattern pattern = Pattern.compile("key_in-(get|set)\\{(\\w*):?(\\w*)\\};");
-        Matcher matcher;
-        String clientmsg, servermsg = ""; //wiadomość od klienta do serwera, wiadomość od serwera do klienta
-        String password = "haslo"; //hasło może zawierać znaki, cyfry i podkreślenie
+        String clientmsg = "", servermsg; //wiadomość od klienta do serwera, wiadomość od serwera do klienta
         ServerSocket ss = new ServerSocket(2011);
-        System.out.println("Waiting for connection...");
         Socket cs = ss.accept();
+        System.out.println("Waiting for connection...");
         if (cs.isConnected())
             System.out.println("Client connected.");
         DataInputStream in = new DataInputStream(cs.getInputStream());
         DataOutputStream out = new DataOutputStream(cs.getOutputStream());
-
         while (true) {
-            clientmsg = in.readUTF();
+            try {
+                clientmsg = in.readUTF();
+            } catch (IOException e) {
+                System.out.println("Connection lost!");
+                closeConnection(in, out, cs, ss);
+                System.exit(1);
+            }
             System.out.println("Client: " + clientmsg);
             if (clientmsg.equals("exit"))
                 break;
-            matcher = pattern.matcher(clientmsg);
-            if (matcher.find()) {
-                servermsg = "key_out-{";
-                if (matcher.group(1).equals("get")) {
-                    servermsg += matcher.group(2);
-                    if (keys.contains(matcher.group(2)))
-                        servermsg += ":yes};";
-                    else
-                        servermsg += ":not};";
-                } else if (matcher.group(1).equals("set")) {
-                    servermsg += matcher.group(3);
-                    if (matcher.group(2).equals(password)) {
-                        keys.add(matcher.group(3));
-                        servermsg += ":yes};";
-                    } else
-                        servermsg += ":not};";
+            servermsg = processMessage(clientmsg);
+            System.out.println("Sending response: " + servermsg);
+            try {
+                out.writeUTF(servermsg);
+            } catch (IOException e) {
+                System.out.println("Connection lost!");
+                closeConnection(in, out, cs, ss);
+                System.exit(1);
             }
-            } else {
-                servermsg ="key_out-{" + clientmsg + ":error};";
-            }
-            out.writeUTF(servermsg);
         }
-        cs.close();
-        in.close();
-        out.close();
+        closeConnection(in, out, cs, ss);
     }
 }
